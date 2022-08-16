d2f50131c742807d8beb68642ca6e0775a876d56
AhmedEltrass <52069792+ahmedeltrass@users.noreply.github.com>
2022-08-16 11:16:00 +0200

    ya mosahel

---
+++ CodeZellApp/FormCell.swift
@@ -0,0 +1,17 @@
+//
+//  FormCell.swift
+//  CodeZellApp
+//
+//  Created by Ahmed Eltrass on 16/08/2022.
+//
+
+import UIKit
+
+class FormCell: UICollectionViewCell {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet weak var slideButton: UIButton!
+}
--- CodeZellApp/ViewController.swift
+++ CodeZellApp/ViewController.swift
@@ -7,11 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "formCell", for: indexPath)
+        return cell
+    }
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return collectionView.frame.size
+//    }
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+                return CGSize(width: (collectionView.frame.width - 20) / 2  , height: collectionView.frame.height)
+            }
+    
 
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
 
 
